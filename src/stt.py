1e57f4efc4861cdb5fe45b789ace9150a3f72e48
Saka1r <sakairlinux@gmail.com>
2026-05-28 15:31:07 +0300

    Added modules for project

---
+++ src/stt.py
@@ -0,0 +1,11 @@
+#hariko
+#author: Saka1r
+# MIT License
+
+class Whisper():
+    def __init__(self):
+        pass
+
+    def load_wav(self):
+        pass
+
---
+++ src/tools.py
@@ -0,0 +1,8 @@
+#hariko
+#author: Saka1r
+#MIT License
+
+class Tools():
+    def convert_to_wav():
+        pass
+    
