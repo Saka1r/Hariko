b36bbfaa2eff6a51bbbde1f9e6136433f42d9d5a
Saka1r <sakairlinux@gmail.com>
2026-06-01 14:05:33 +0300

    Created a screen "setting".

--- src/hariko.py
+++ src/hariko.py
@@ -11,10 +11,33 @@
 from kivymd.app import MDApp
 from kivy.lang.builder import Builder
 
+from kivymd.uix.navigationbar import MDNavigationBar, MDNavigationItem
+
 class Hariko(MDApp):
+
+    def on_switch_tabs(
+        self,
+        bar: MDNavigationBar,
+        item: MDNavigationItem,
+        item_icon: str,
+        item_text: str,
+    ):
+        self.root.ids.screen_manager.current = item_text
+
+    def switch_theme(self, active):
+        if active:
+            self.theme_cls.theme_style = "Light"
+        else:
+            self.theme_cls.theme_style = "Dark"
+
+    
+
     def build(self):
+
+        self.theme_cls.theme_style_switch_animation = True
+
         self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Purple"
+        self.theme_cls.primary_palette = "Blue"
         return Builder.load_file("hariko.kv")
 
 if __name__ == '__main__':
